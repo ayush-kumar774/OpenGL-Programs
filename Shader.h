1d839a40f4513fdfc306d1ff64c722ad3e8b8dd8
Aayush Kumar <53551688+ayush-kumar774@users.noreply.github.com>
2020-11-18 13:18:38 +0530

    It compiles the shader of triangle.cpp and link it.

--- Shader.h
+++ Shader.h
@@ -24,6 +24,7 @@ static unsigned int CompileShader(unsigned int type, const std::string &source)
         }
 
         return id;
+        
 }
 
 static unsigned int CreateShader(const std::string vertexShader, const std::string fragmentShader)
